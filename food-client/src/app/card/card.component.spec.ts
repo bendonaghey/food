ea0b8dfdbcb020c510de74ab436b7490ac4fee8d
Niall Conaghan <niall.conaghan@gmail.com>
2018-06-15 07:58:42 +0100

    Fixed failing tests after OR merge

--- food-client/src/app/card/card.component.spec.ts
+++ food-client/src/app/card/card.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CardComponent } from './card.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Post } from '../models/post.model';
 
 describe('CardComponent', () => {
   let component: CardComponent;
@@ -18,6 +19,9 @@ describe('CardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CardComponent);
     component = fixture.componentInstance;
+
+    component.card = <Post>{};
+    
     fixture.detectChanges();
   });
 
