09257e3da6bdd4fee06cf1c60f5c5a4f70510a2d
Niall Conaghan <niall.conaghan@gmail.com>
2018-06-15 10:47:23 +0100

    removed consol.log

--- food-service/api/post/post.service.js
+++ food-service/api/post/post.service.js
@@ -3,7 +3,6 @@ var post = require('../models/post');
 module.exports = {
   getAllPosts: function(callback) {
     post.find({}, function(error, posts) {
-      console.log(posts);
       if (error) {
         console.log(error);
         return;
