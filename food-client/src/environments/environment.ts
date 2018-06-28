10cd7256a106f9ef8181136316a40c9293c3ff1f
Niall Conaghan <niall.conaghan@gmail.com>
2018-06-28 09:08:21 +0100

    remove data

--- food-client/src/environments/environment.ts
+++ food-client/src/environments/environment.ts
@@ -4,12 +4,7 @@
 
 export const environment = {
   production: false,
-  apiKey: 'AIzaSyBWhBvN22AYR1FAVl7wcFENLenFTaCn8a4',
-  authDomain: 'foodshare-d7200.firebaseapp.com',
-  databaseURL: 'https://foodshare-d7200.firebaseio.com/',
-  projectId: 'foodshare-d7200',
-  storageBucket: 'foodshare-d7200.appspot.com',
-  messagingSenderId: '420100614310'
+
 };
 
 /*
