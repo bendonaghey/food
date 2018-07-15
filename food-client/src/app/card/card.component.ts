20ae45b3816d1f284b492e476ed8066386bca112
Niall <niall.conaghan@gmail.com>
2018-07-15 21:01:11 +0100

    basic animation for card and route to view post

--- food-client/src/app/card/card.component.ts
+++ food-client/src/app/card/card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Post } from '../models/post.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-card',
@@ -9,7 +10,14 @@ import { Post } from '../models/post.model';
 export class CardComponent implements OnInit {
   @Input() card: Post;
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit() {}
+
+  viewPost(post: Post) {
+    this.router.navigate(['/posts', post.postId]);
+    console.log(post);
+  }
+
+  likePost(post: Post) {}
 }
