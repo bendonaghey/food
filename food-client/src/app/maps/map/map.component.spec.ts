c2faf34e32d54523b61baa94c9e61616459860ec
Niall <niall.conaghan@gmail.com>
2018-07-28 20:35:03 +0100

    fixed tests

--- food-client/src/app/maps/map/map.component.spec.ts
+++ food-client/src/app/maps/map/map.component.spec.ts
@@ -8,12 +8,19 @@ import { MapsAPILoader } from '@agm/core';
 describe('MapComponent', () => {
   let component: MapComponent;
   let fixture: ComponentFixture<MapComponent>;
+  let mockMapsAPILoader: any;
+
 
   beforeEach(async(() => {
+    mockMapsAPILoader = jasmine.createSpyObj('mapsAPILoader', ['load']);
+    mockMapsAPILoader.load.and.returnValue({then: () => {}});
     TestBed.configureTestingModule({
       declarations: [MapComponent],
       imports: [ReactiveFormsModule],
-      providers: [MapsAPILoader],
+      providers: [
+        MapsAPILoader,
+        { provide: MapsAPILoader, useValue: mockMapsAPILoader }
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   }));
